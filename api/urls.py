1735dd2e8651ac28818973e84910a1759ad6cf67
abl1v1on <abl1v1on@yandex.ru>
2024-05-13 21:03:49 +0500

    created endpoints for api

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from . import views
+
 
 app_name = 'api_v1'
 
-urlpatterns = [
 
+urlpatterns = [
+    path('token/', views.MyTokenView.as_view(), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/', views.SignUpUserView.as_view(), name='signup_user'),
+    path('notes/', views.UserNotesListView.as_view(), name='user_notes_list'),
+    path('notes/<int:pk>/', views.UserNoteRetriveView.as_view(), name='user_note'),
 ]
