06483ba3e63ff006deec294d9b154da9dfb6896e
abl1v1on <abl1v1on@yandex.ru>
2024-05-13 16:41:08 +0500

    change note utils methods

--- app/utils.py
+++ app/utils.py
@@ -1,38 +1,22 @@
-from django.shortcuts import redirect
-
 from .models import Note
 
 
 class NoteUtils:
-    def activate_note(self, post_data):
-        note_id = post_data.get('activate_note')
-        note_obj = self.__get_note_object(note_id)
-        note_obj.is_active = True
-        note_obj.save()
+    def __init__(self, model) -> None:
+        self.model = model
 
-    def complete_note(self, post_data):
-        note_id = post_data.get('complete_note')
-        note_obj = self.__get_note_object(note_id)
-        note_obj.is_active = False
-        note_obj.save()
+    def get_note_object(self, note_id: int):
+        return self.model.objects.get(pk=note_id)
 
-    def delete_note(self, note_id: int):
-        note_obj = self.__get_note_object(note_id)
-        note_obj.delete()
+    def get_user_notes(self, user_id: int):
+        return self.model.objects.filter(author_id=user_id)
 
+    def create_note(self, name: str, user):
+        return self.model.objects.create(name=name, author=user)
+    
     @staticmethod
-    def __get_note_object(note_id: int):
-        return Note.objects.get(pk=note_id)
+    def delete_note(note: Note):
+        return note.delete()
+    
 
-    @staticmethod
-    def get_user_notes(user_id: int):
-        return Note.objects.filter(author_id=user_id)
-
-    # @staticmethod
-    # def create_note(name: str, user):
-    #     note = Note.objects.create(name=name, author=user)
-    #     note.save()
-    #     return redirect('app:home')
-
-
-note_utils = NoteUtils()
+note_utils = NoteUtils(Note)
