2a88aa0ff269bff643f1a2fb0c72149e07af6c93
abl1v1on <abl1v1on@yandex.ru>
2024-05-13 21:04:29 +0500

    created views for signup, token, user notes

--- api/views.py
+++ api/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from app.models import Note
+from .serializers import CreateUserSerializer, MyTokenSerializer, NoteSerializer
+
+
+class MyTokenView(TokenObtainPairView):
+    serializer = MyTokenSerializer
+
+
+class SignUpUserView(CreateAPIView):
+    serializer_class = CreateUserSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = (AllowAny, )
+
+
+class UserNotesListView(ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        return Note.objects.filter(author=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        serializer = NoteSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UserNoteRetriveView(RetrieveUpdateDestroyAPIView):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        try:
+            note = Note.objects.get(pk=kwargs['pk'])
+
+            if note.author == request.user:
+                serializer = NoteSerializer(data=note.__dict__)
+                serializer.is_valid(raise_exception=True)
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            return Response({'detail': 'Access is denied'}, status=status.HTTP_400_BAD_REQUEST)  
+        except Note.DoesNotExist:
+            return Response({'detail': 'Note not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            note = Note.objects.get(pk=kwargs['pk'])
+
+            if note.author == request.user:
+                return super().delete(request, *args, **kwargs)
+            return Response({'detail': 'Access is denied'}, status=status.HTTP_400_BAD_REQUEST)    
+        
+        except Note.DoesNotExist:
+            return Response({'detail': 'Note not found'}, status=status.HTTP_404_NOT_FOUND)
+        
